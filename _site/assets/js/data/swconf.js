bde8c34b97080c35feb28b1db5696bb4455fcff8
sanyinchen <sanyinchen@baidu.com>
2025-06-19 20:19:34 +0800

    add authors

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1750335192',resources: [
+    cacheName: 'chirpy-1750335540',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
