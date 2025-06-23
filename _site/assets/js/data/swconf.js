bde5f15a9a5211e116a41b95e28f1a8eef4f635a
sanyinchen <sanyinchen@baidu.com>
2025-06-23 16:33:53 +0800

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1750590617',resources: [
+    cacheName: 'chirpy-1750667625',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
